786c83791f30ccb1f58b06915c13ba25dc74fd36
stan <morfij@gmail.com>
2015-10-25 00:34:46 +0200

    still configuring gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,58 @@
 var
  gulp = require('gulp'),
  imagemin = require('gulp-imagemin'),
- newer = require('gulp-newer');
+ newer = require('gulp-newer'),
+ htmlclean = require('gulp-htmlclean'),
+ preprocess = require('gulp-preprocess'),
+ del = require('del'),
+ pkg = require('./package.json');
+
 
 // file locations
 var
+devBuild = ((process.env.NODE_ENV || 'development').trim().toLowerCase() !== 'production'),
+
 source = 'source/',
-dest = 'build/';
+dest = 'build/',
+
+html = {
+	in: source + '*.html',
+	watch: [source + '*html', source + 'template/**/*'],
+	out: dest,
+	context: {
+	 devBuild: devBuild,
+	 author: pkg.author,
+	 version: pkg.version
+	}
+},
 
 images = {
 	in: source + 'images/*.*',
 	out: dest + 'images/'
 };
+
+
+// show build type
+ console.log(pkg.name + '  ' + pkg.version + ', ' + (devBuild ? 'development' : 'production') + ' build ');
+
+// clean the build directory
+ gulp.task('clean', function(){
+ del([
+	dest + '*' 
+ ]);
+});
+
+
+// build HTML files
+gulp.task('html', function(){
+var page = gulp.src(html.in)
+	.pipe(preprocess({ context: html.context }));
+	if (!devBuild){
+		page = page.piep(htmlclean());
+	}
+	return page.pipe(gulp.dest(html.out));
+
+});
 
 
 //manage images
@@ -26,6 +67,11 @@ gulp.task('images', function(){
 });
 
 //default task
-gulp.task('default', function(){
+gulp.task('default', ['html', 'images'], function(){
 
+// html changes
+gulp.watch(html.watch, ['html']);
+
+// image changes
+	gulp.watch(images.in, ['images']);
 });
